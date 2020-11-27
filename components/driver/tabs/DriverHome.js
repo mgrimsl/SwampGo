197d5609a5df340b868f8a535c5c9b61be244dea
mgrimsl <60195200+mgrimsl@users.noreply.github.com>
2020-11-27 11:23:42 -0500

    added first fetch

--- components/driver/tabs/DriverHome.js
+++ components/driver/tabs/DriverHome.js
@@ -6,7 +6,12 @@ import { View, Text} from 'react-native';
 const DriverHome = ({ navigation, route }) => {
 
 
-
+  fetch("http://54.162.93.0/swampApi/", {
+    method : 'GET'
+  })
+  .then(res => res.json())
+  .then(data => {console.log(data)})
+  .catch(err => console.error(err))
   return (
     <View>
         <Text>Home</Text>
